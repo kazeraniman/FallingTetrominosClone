f0b1322756e26e9cf4b94bc500f745f467b59889
Iman Kazerani <Kazeraniman@gmail.com>
2018-05-05 15:24:26 -0400

    Basic active piece movement.

--- Scenes/Grid/Grid.gd
+++ Scenes/Grid/Grid.gd
@@ -44,7 +44,7 @@ func _ready():
 			new_grid_cell.position = Vector2(GRID_CELL_INITIAL_HORIZONTAL_OFFSET + GRID_CELL_SIZE * column , GRID_CELL_INITIAL_VERTICAL_OFFSET + GRID_CELL_SIZE * row)
 			grid_cells[row][column] = new_grid_cell
 			add_child(new_grid_cell)
-	
+
 	# TEMP
 	new_tetromino = Tetromino.instance()
 	add_child(new_tetromino)
@@ -53,11 +53,55 @@ func _process(delta):
 	# Update the textures for all of the grid cells
 	for row in range(PADDED_NUM_ROWS):
 		for column in range(PADDED_NUM_COLUMNS):
-			if grid_state[row][column] != Utility.INVALID:
+			# Only draw for valid cells
+			if row >= GRID_PAD and row < PADDED_NUM_ROWS - GRID_PAD and column >= GRID_PAD and column < PADDED_NUM_COLUMNS - GRID_PAD:
 				grid_cells[row - GRID_PAD][column - GRID_PAD].set_cell_type(grid_state[row][column])
-	
+
 	# Update the textures for the active piece
 	for row in range(new_tetromino.piece_matrix.size()):
 		for column in range(new_tetromino.piece_matrix.size()):
 			if new_tetromino.piece_matrix[row][column] == Utility.PIECE:
-				grid_cells[row + active_piece_top_left_anchor.y - GRID_PAD][column + active_piece_top_left_anchor.x - GRID_PAD].set_cell_type(active_piece_type)
+				# Only draw for valid cells
+				var cell_to_draw = Vector2(column + active_piece_top_left_anchor.x, row + active_piece_top_left_anchor.y)
+				if cell_to_draw.y >= GRID_PAD and cell_to_draw.y < PADDED_NUM_ROWS - GRID_PAD and cell_to_draw.x >= GRID_PAD and cell_to_draw.x < PADDED_NUM_COLUMNS - GRID_PAD:
+					grid_cells[cell_to_draw.y - GRID_PAD][cell_to_draw.x - GRID_PAD].set_cell_type(active_piece_type)
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("move_down"):
+		try_move(Vector2(0, 1))
+	# UP IS ONLY FOR TESTING PURPOSES, SHOULD BE REMOVED LATER
+	if Input.is_action_just_pressed("move_up"):
+		try_move(Vector2(0, -1))
+	if Input.is_action_just_pressed("move_left"):
+		try_move(Vector2(-1, 0))
+	if Input.is_action_just_pressed("move_right"):
+		try_move(Vector2(1, 0))
+
+func try_move(desired_move):
+	"""
+	Try to move the active piece in the desired direction.
+	:param desired_move: The direction in which the move is intended along both axes.
+	:type desired_move: Vector2.
+	:return: True if the move is successful, false otherwise.
+	:rtype: Boolean.
+	"""
+	# Try the move out by generating the associated position
+	var attempted_move_top_left_anchor = active_piece_top_left_anchor + desired_move
+	var acceptable_move = true
+
+	# Check the piece positions to ensure that there is no overlap at any point
+	for row in range(new_tetromino.piece_matrix.size()):
+		if acceptable_move:
+			for column in range(new_tetromino.piece_matrix.size()):
+				if new_tetromino.piece_matrix[row][column] == Utility.PIECE:
+					if grid_state[row + attempted_move_top_left_anchor.y][column + attempted_move_top_left_anchor.x] != Utility.EMPTY:
+						acceptable_move = false
+						break;
+
+	# If the move causes no overlap, perform it
+	if acceptable_move:
+		active_piece_top_left_anchor = attempted_move_top_left_anchor
+		return true
+	# Otherwise reject the move
+	else:
+		return false
