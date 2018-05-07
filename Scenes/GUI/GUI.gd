325ab14a0c2bc3b468ef64b17f688b6d5e9f4744
Iman Kazerani <Kazeraniman@gmail.com>
2018-05-07 16:55:47 -0400

    Adding in the GUI controls to display a held block and cleaning up the loading of the block displays for the GUI.

--- Scenes/GUI/GUI.gd
+++ Scenes/GUI/GUI.gd
@@ -2,14 +2,6 @@ extends CanvasLayer
 
 var Utility = preload("res://Scripts/Utility.gd")
 
-var IBlock = preload("res://Art/Tetrominos/IBlock.png")
-var JBlock = preload("res://Art/Tetrominos/JBlock.png")
-var LBlock = preload("res://Art/Tetrominos/LBlock.png")
-var OBlock = preload("res://Art/Tetrominos/OBlock.png")
-var SBlock = preload("res://Art/Tetrominos/SBlock.png")
-var TBlock = preload("res://Art/Tetrominos/TBlock.png")
-var ZBlock = preload("res://Art/Tetrominos/ZBlock.png")
-
 func set_score(score):
 	"""
 	Sets the score on the GUI to the provided value.
@@ -28,24 +20,16 @@ func set_cleared_lines(cleared_lines):
 
 func set_next_block(next_block):
 	"""
-	Set's the texture of next block on the GUI to the block image matching the input.
+	Sets the texture of next block on the GUI to the block image matching the input.
 	:param next_block: The block type to be displayed.
 	:type next_block: GridValues.
 	"""
-	match next_block:
-		Utility.EMPTY:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = null
-		Utility.IBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = IBlock
-		Utility.JBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = JBlock
-		Utility.LBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = LBlock
-		Utility.OBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = OBlock
-		Utility.SBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = SBlock
-		Utility.TBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = TBlock
-		Utility.ZBLOCK:
-			$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = ZBlock
+	$NextTetrominoPanel/VBoxContainer/NextTetrominoImage.texture = Utility.TETROMINO_TEXTURES[next_block]
+
+func set_hold_block(held_block):
+	"""
+	Sets the texture of held block on the GUI to the block image matching the input.
+	:param held_block: The block type to be displayed.
+	:type held_block: GridValues.
+	"""
+	$HeldTetrominoPanel/VBoxContainer/HeldTetrominoImage.texture = Utility.TETROMINO_TEXTURES[held_block]
--- Scripts/Utility.gd
+++ Scripts/Utility.gd
@@ -1,7 +1,24 @@
 extends Node
 
+const I_BLOCK_TEXTURE = preload("res://Art/Tetrominos/IBlock.png")
+const J_BLOCK_TEXTURE = preload("res://Art/Tetrominos/JBlock.png")
+const L_BLOCK_TEXTURE = preload("res://Art/Tetrominos/LBlock.png")
+const O_BLOCK_TEXTURE = preload("res://Art/Tetrominos/OBlock.png")
+const S_BLOCK_TEXTURE = preload("res://Art/Tetrominos/SBlock.png")
+const T_BLOCK_TEXTURE = preload("res://Art/Tetrominos/TBlock.png")
+const Z_BLOCK_TEXTURE = preload("res://Art/Tetrominos/ZBlock.png")
+
 enum GridValues { EMPTY, PIECE, INVALID, IBLOCK, JBLOCK, LBLOCK, OBLOCK, SBLOCK, TBLOCK, ZBLOCK }
 enum RotationDirections { LEFT = -1, RIGHT = 1 }
 enum PieceState { STANDBY, ACTIVE }
 
 const tetromino_types = [ IBLOCK, JBLOCK, LBLOCK, OBLOCK, SBLOCK, TBLOCK, ZBLOCK ]
+const TETROMINO_TEXTURES = {
+	IBLOCK: I_BLOCK_TEXTURE,
+	JBLOCK: J_BLOCK_TEXTURE,
+	LBLOCK: L_BLOCK_TEXTURE,
+	OBLOCK: O_BLOCK_TEXTURE,
+	SBLOCK: S_BLOCK_TEXTURE,
+	TBLOCK: T_BLOCK_TEXTURE,
+	ZBLOCK: Z_BLOCK_TEXTURE,
+}
